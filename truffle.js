44ce12160f451b291acabdc07b7f3569ba70d826
Steve Mulligan <steve@nonlocal.ca>
2018-08-09 15:31:22 -0400

    updated networks for deployment script

--- truffle.js
+++ truffle.js
@@ -4,7 +4,7 @@ var Web3 = require("web3");
 var ProviderEngine = require("web3-provider-engine");
 var FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
 var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
-var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
+var Web3Subprovider = require("web3-provider-engine/subproviders/provider.js");
 
 // create wallet from existing private key
 var privateKey = secrets.config.myPrivateKey;
@@ -12,47 +12,38 @@ var wallet = ethereumjsWallet.fromPrivateKey(new Buffer(privateKey, "hex"));
 
 // mainnet
 var providerURL = {
-    Main: "https://mainnet.infura.io/" + secrets.config.infuraKey,
-    Ropsten: "https://ropsten.infura.io/" + secrets.config.infuraKey,
-    Rinkeby: "https://rinkeby.infura.io/" + secrets.config.infuraKey
+	Ellaism: "https://jsonrpc.ellaism.org",
+	Shikinseki: "https://jsonrpc.testnet.ellaism.org"
 };
 var engines = {
-    Main: new ProviderEngine(),
-    Ropsten: new ProviderEngine(),
-    Rinkeby: new ProviderEngine()
+    Ellaism: new ProviderEngine(),
+	  Shikinseki: new ProviderEngine()
 };
-var networks = ["Main", "Ropsten", "Rinkeby"];
-for (let i=0; i<networks.length; i++) {
+var networks = ["Ellaism", "Shikinseki"];
+for (let i in networks) {
     engines[networks[i]].addProvider(new FilterSubprovider());
     engines[networks[i]].addProvider(new WalletSubprovider(wallet, {}));
     engines[networks[i]].addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(providerURL[networks[i]])));
     engines[networks[i]].start();
 }
-var gas = 4712388;
+var gas = 4700000;
 var gasPrice = 1.5*1e9;
 
 module.exports = {
     networks: {
-        infuraMain: {
-            network_id: 1,
-            provider: engines["Main"],
-            gas: gas,
-            gasPrice: gasPrice,
-            from: secrets.config.myAddress
-        },
-        infuraRopsten: {
-            network_id: 3,
-            provider: engines["Ropsten"],
-            gas: gas/2,
-            gasPrice: gasPrice,
-            from: secrets.config.myAddress
-        },
-        infuraRinkeby: {
-            network_id: 4,
-            provider: engines["Rinkeby"],
-            gas: gas,
-            gasPrice: gasPrice,
-            from: secrets.config.myAddress
-        }
+        ellaism: {
+			    network_id: 64,
+			    provider: engines["Ellaism"],
+			    gas: gas,
+			    gasPrice: gasPrice,
+			    from: secrets.config.myAddress
+		    },
+	      shikinseki: {
+			    network_id: 16448,
+			    provider: engines["Shikinseki"],
+			    gas: gas,
+			    gasPrice: gasPrice,
+			    from: secrets.config.myAddress
+		    }
     }
 };
